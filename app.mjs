6cc93c9bb661f3d0b3b843ce4a7df5ebf60a7920
Carlos V <git@valera.fyi>
2023-10-10 01:25:00 -0700

    Mobile-friendly styling and text input for volume.

--- app.mjs
+++ app.mjs
@@ -40,15 +40,15 @@ gFrequencyInputText.addEventListener("input", (e) => {
   console.debug(e);
 
   let freq;
-  gFrequencyInputText.setCustomValidity("");
+  e.target.setCustomValidity("");
   try {
-    freq = Frequency.parse(gFrequencyInputText.value);
+    freq = Frequency.parse(e.target.value);
   } catch (err) {
     if (!(err instanceof NoteError)) throw err;
-    gFrequencyInputText.setCustomValidity(err.message);
+    e.target.setCustomValidity(err.message);
     return;
   } finally {
-    gFrequencyInputText.reportValidity();
+    e.target.reportValidity();
   }
 
   gFrequencyEventTarget.dispatchFrequency(freq);
@@ -58,7 +58,8 @@ gFrequencyInputText.addEventListener("input", (e) => {
 
 gFrequencyInputSlider.addEventListener("input", (e) => {
   console.debug(e);
-  gSliderNote.index = gFrequencyInputSlider.value;
+
+  gSliderNote.index = e.target.value;
   gFrequencyEventTarget.dispatchFrequency(gSliderNote.frequency);
   const hz = gSliderNote.frequency.hz;
   gFrequencyInputText.value =
@@ -71,17 +72,12 @@ gFrequencyInputSlider.addEventListener("input", (e) => {
 const gPlayPauseButton = document.querySelector("#playPauseButton");
 /** @type {HTMLSelectElement} */
 const gFunctionKindSelect = document.querySelector("#functionKindSelect");
-/** @type {HTMLInputElement} */
-const gVolumeSlider = document.querySelector("#volumeSlider");
 
 /** @type {?AudioContext} */
 var gAudioContext = null;
 
 /** @type {?OscillatorNode} */
 var gOscillator = null;
-
-/** @type {?GainNode} */
-var gGainNode = null;
 
 /** @type {Frequency} */
 var gLatestValidFrequency = new Frequency();
@@ -93,6 +89,100 @@ gFrequencyEventTarget.addEventListener('FrequencyChange', (
   }
 });
 
+class VolumeInput {
+  /** @type {?GainNode} */
+  gainNode = null;
+
+  /** @type {HTMLInputElement} */
+  #inputSlider = document.querySelector("#volumeInputSlider");
+  /** @type {HTMLInputElement} */
+  #inputText = document.querySelector("#volumeInputText");
+
+  constructor() {
+    this.#inputSlider.addEventListener('input', (e) => this.#onSliderInput(e));
+    this.#inputText.addEventListener('input', (e) => this.#onTextInput(e));
+  }
+
+  /**
+   * Initializes the gainNode by using the given audioContext.
+   * If the gainNode is already initialized, does nothing.
+   *
+   * @param {AudioContext} ctx
+   */
+  initGainNode(ctx) {
+    if (this.gainNode != null) return;
+
+    this.gainNode = ctx.createGain();
+    this.#updateGainParam();
+    this.gainNode.connect(ctx.destination);
+  }
+
+  get value() { return this.#inputSlider.value; }
+  get max() { return this.#inputSlider.max; }
+  get min() { return this.#inputSlider.min; }
+
+  /**
+   * Updates the internal gainNode's param based on the current slider value.
+   */
+  #updateGainParam() {
+    let newGain = (this.value - this.min) / (this.max - this.min);
+    newGain = newGain * newGain;  // Approximate logarithmic behavior.
+    console.debug(`Setting gain to ${newGain}`);
+    if (this.gainNode != null) {
+      this.gainNode.gain.value = newGain;
+    }
+    return newGain;
+  }
+
+  /** @param {Event} e */
+  #onSliderInput(e) {
+    this.#inputText.value = `${e.target.value}`;
+    this.#updateGainParam();
+  }
+  /** @param {Event} e */
+  #onTextInput(e) {
+    console.debug(e)
+
+    let parsed;
+    e.target.setCustomValidity("");
+    try {
+      parsed = this.#parseVolume(e.target.value)
+    } catch (err) {
+      e.target.setCustomValidity(err.message);
+      return;
+    } finally {
+      e.target.reportValidity();
+    }
+
+    this.#inputSlider.value = parsed;
+    this.#updateGainParam();
+  }
+
+  /**
+   * @param {string} text The volume integer to parse. [0, 100].
+   * @returns {Number}
+   */
+  #parseVolume(text) {
+    let parsed;
+    if (text == "") {
+      parsed = 0;
+    } else {
+      parsed = parseInt(text, 10);
+    }
+
+    if (Number.isNaN(parsed)) {
+      throw new Error(`Failed to parse volume to number: ${text}`);
+    }
+    if (parsed < 0 || parsed > 100) {
+      throw new Error(`Volume out of range: ${parsed}. Should be [0, 100].`)
+    }
+
+    return parsed;
+  }
+}
+
+const gVolumeInput = new VolumeInput();
+
 /**
  * @param {HTMLInputElement} slider
  * @returns {Number}
@@ -102,16 +192,6 @@ function computeGain(slider) {
   newGain = newGain * newGain;
   return newGain;
 }
-
-gVolumeSlider.addEventListener('input', (e) => {
-  if (gGainNode == null) {
-    return;
-  }
-
-  let newGain = computeGain(e.target);
-  console.debug(`Setting gain to ${newGain}`);
-  gGainNode.gain.value = newGain;
-});
 
 gFunctionKindSelect.addEventListener('input', (e) => {
   if (gOscillator == null) {
@@ -140,17 +220,13 @@ gPlayPauseButton.addEventListener('click', (e) => {
   }
   const ctx = gAudioContext;
 
-  if (gGainNode == null) {
-    gGainNode = ctx.createGain();
-    gGainNode.gain.value = computeGain(gVolumeSlider);
-    gGainNode.connect(ctx.destination);
-  }
+  gVolumeInput.initGainNode(ctx);
 
   if (gOscillator == null) {
     gOscillator = ctx.createOscillator();
     gOscillator.type = gFunctionKindSelect.value || 'sine';
     gOscillator.frequency.value = gLatestValidFrequency.hz;
-    gOscillator.connect(gGainNode);
+    gOscillator.connect(gVolumeInput.gainNode);
   }
 
   gOscillator.start();
